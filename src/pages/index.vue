d1968e3fe98752dc7767e6ba115123298433f526
Łukasz Samcik <l.samcik@gmail.com>
2023-07-25 20:39:40 +0200

    adding favicon and stylind the app

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts">
-// const { t } = useI18n()
-// const userStore = useUserStore()
-// const weatherStore = useWeatherStore()
-// weatherStore.loadWeather()
 const inputValue = ref()
 const apiKey = '4cda0b1231a98ddce2d7662e79d6877b'
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
-
 const data = ref()
 
 async function checkWeather() {
@@ -20,43 +15,39 @@ function sendCityName() {
 </script>
 
 <template>
-  <!-- <div>
-    <h1>{{ weatherStore.weather.latitude }} </h1>
-  </div> -->
-
   <div class="box max-w-400px rounded-10px px-10px py-20px">
     <div class="flex items-center justify-between gap-2">
-      <input v-model="inputValue" class="h-60px rounded-30px border-none bg-#ebfffc px-25px py-10px outline-none" type="text" placeholder="enter city name" spellcheck="false">
+      <input v-model="inputValue" class="h-60px w-full rounded-30px border-none bg-#ebfffc px-25px py-10px outline-none" type="text" placeholder="enter city name" spellcheck="false">
       <button class="rounded-full bg-#ebfffc p-4" @click="sendCityName">
         <div class="i-mdi:magnify text-20px" />
       </button>
     </div>
     <div v-if="data">
       <div class="w-full flex flex-col items-center text-white">
-        <img v-if="data.weather[0].main === 'Rain'" class="max-w-150px" src="public/rain.png" alt="">
-        <img v-if="data.weather[0].main === 'Clouds'" class="max-w-150px" src="public/clouds.png" alt="">
-        <img v-if="data.weather[0].main === 'Clear'" class="max-w-150px" src="public/clear.png" alt="">
-        <img v-if="data.weather[0].main === 'Drizzle'" class="max-w-150px" src="public/drizzle.png" alt="">
-        <img v-if="data.weather[0].main === 'Mist'" class="max-w-150px" src="public/mist.png" alt="">
-        <img v-if="data.weather[0].main === 'Snow'" class="max-w-150px" src="public/snow.png" alt="">
-
+        <img v-if="data.weather[0].main === 'Rain'" class="max-w-150px" src="/rain.png" alt="">
+        <img v-if="data.weather[0].main === 'Clouds'" class="max-w-150px" src="/clouds.png" alt="">
+        <img v-if="data.weather[0].main === 'Clear'" class="max-w-150px" src="/clear.png" alt="">
+        <img v-if="data.weather[0].main === 'Drizzle'" class="max-w-150px" src="/drizzle.png" alt="">
+        <img v-if="data.weather[0].main === 'Mist'" class="max-w-150px" src="/mist.png" alt="">
+        <img v-if="data.weather[0].main === 'Snow'" class="max-w-150px" src="/snow.png" alt="">
         <h1 class="text-50px">
           {{ data.name }}
         </h1>
-        <h2 class="text-20px">
+        <h2>{{ data.weather[0].description }}</h2>
+        <h2 class="mt-4 text-30px">
           {{ Math.round(data.main.temp) }} C°
         </h2>
       </div>
       <div class="mx-20px mt-10 flex items-center justify-between text-white">
         <div class="flex flex-col items-center gap-2">
-          <img class="max-w-50px" src="public/humidity.png" alt="">
+          <img class="max-w-50px" src="/humidity.png" alt="">
           <div>
             <p>{{ data.main.humidity }} %</p>
             <p>Humidity</p>
           </div>
         </div>
         <div class="flex flex-col items-center gap-2">
-          <img class="max-w-50px" src="public/wind.png" alt="">
+          <img class="max-w-50px" src="/wind.png" alt="">
           <div>
             <p>{{ data.wind.speed }} km/h</p>
             <p>Wind Speed</p>
